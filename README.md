9a72dca32ae3d8993cfdde2530696af30aff7ad0
miguel <miguelsodre007@gmail.com>
2020-12-04 10:37:12 -0300

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# TikTok-Clone
+Clone do TikTok com direito a darkmode. 😍
+
+## Techs
+
+- [x] NextJs
+- [x] Styled Components
+- [x] MongoDB
+
+## Images
+
+![enter image description here](https://github.com/MiguelS007/Github-Clone/blob/main/src/assets/WhatsApp%20Image%202020-11-18%20at%2014.44.46.jpeg?raw=true)
+![enter image description here](https://github.com/MiguelS007/Github-Clone/blob/main/src/assets/WhatsApp%20Image%202020-11-18%20at%2014.45.29.jpeg?raw=true)
+![enter image description here](https://github.com/MiguelS007/Github-Clone/blob/main/src/assets/WhatsApp%20Image%202020-11-18%20at%2014.45.14.jpeg?raw=true)
+![enter image description here](https://github.com/MiguelS007/Github-Clone/blob/main/src/assets/WhatsApp%20Image%202020-11-18%20at%2014.45.42.jpeg?raw=true)
+
+## Participants
+
+[<img src="https://avatars3.githubusercontent.com/u/61780220?s=400&u=f70299eb4ea11b4db2643818c9dfe4185e5898c7&v=4" width="75px;"/>](https://github.com/MiguelS007)
+
+[Miguel Sodré](https://github.com/MiguelS007)
+
+## Starting Dev Environment
+
+1. Run `npm install` or `yarn install`.<br />
+2. Run `yarn start` and access `http://localhost:3000`.<br />
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests.
+
+### This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -28,10 +28,22 @@ const people = [
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
-    name: 'Anitta',
-    username: '@anitta',
+    name: 'Isis Valverde',
+    username: '@isisvalverde',
     avatar:
-      'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1601125200&x-signature=eEiuYnl%2Fl3JG0Glicyoe9lX8M0A%3D',
+      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
+  },
+  {
+    name: 'Neymar Jr',
+    username: '@neymarjr',
+    avatar:
+      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
+  },
+  {
+    name: 'Felipe Neto',
+    username: '@felipeneto',
+    avatar:
+      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
     name: 'Isis Valverde',
@@ -52,12 +64,6 @@ const people = [
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
-    name: 'Anitta',
-    username: '@anitta',
-    avatar:
-      'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
-  },
-  {
     name: 'Isis Valverde',
     username: '@isisvalverde',
     avatar:
@@ -74,36 +80,6 @@ const people = [
     username: '@felipeneto',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
-  },
-  {
-    name: 'Anitta',
-    username: '@anitta',
-    avatar:
-      'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
-  },
-  {
-    name: 'Isis Valverde',
-    username: '@isisvalverde',
-    avatar:
-      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
-  },
-  {
-    name: 'Neymar Jr',
-    username: '@neymarjr',
-    avatar:
-      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
-  },
-  {
-    name: 'Felipe Neto',
-    username: '@felipeneto',
-    avatar:
-      'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
-  },
-  {
-    name: 'Anitta',
-    username: '@anitta',
-    avatar:
-      'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
   },
   {
     name: 'Isis Valverde',
